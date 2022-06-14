ac373a8ca967e2f9c826991f79179ce1861e6cb1
vnikitinsky <v.nikitinsky@gmail.com>
2022-06-14 03:34:46 +0300

    Minor upd

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -4,15 +4,12 @@ import { Link } from "gatsby"
 import Layout from "./layout"
 import Title from "./title"
 import Listing from "./listing"
-import List from "./list"
 import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
 import useSiteMetadata from "../hooks/use-site-metadata"
 import replaceSlashes from "../utils/replaceSlashes"
 import { visuallyHidden } from "../styles/utils"
 // @ts-ignore
 import Hero from "../texts/hero"
-// @ts-ignore
-import Bottom from "../texts/bottom"
 
 type PostsProps = {
   posts: {
@@ -40,13 +37,11 @@ const Homepage = ({ posts }: PostsProps) => {
       <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 }, variant: `section_hero` }}>
         <Hero />
       </section>
-      <Title text="Статьи">
+      <Title text="Новое в блоге">
         <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>Все статьи</Link>
       </Title>
       <Listing posts={posts} showTags={false} />
-      <List>
-        <Bottom />
-      </List>
+      
     </Layout>
   )
 }
